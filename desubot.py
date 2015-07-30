c51e855046c97ae407b266c6bff999183c25eeb5
Motoko11 <robertswaine7@gmail.com>
2015-07-30 21:47:45 +0100

    Remove silly self import

--- desubot.py
+++ desubot.py
@@ -1,8 +1,7 @@
 from motobot import IRCBot
-import desubot as self
 
+
+bot = IRCBot('desubot', 'irc.rizon.net')
 
 if __name__ == '__main__':
-    self.bot.run()
-else:
-    bot = IRCBot('desubot', 'irc.rizon.net')
+    bot.run()
