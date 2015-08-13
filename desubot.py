34a2b3a93bd96643d74fcb3c8d2f8db52d18253f
Motoko11 <robertswaine7@gmail.com>
2015-08-14 00:45:37 +0100

    Make exception on reload not crash input

--- desubot.py
+++ desubot.py
@@ -1,6 +1,7 @@
 from motobot.irc_bot import IRCBot, IRCLevel
 import desubot
 import threading
+import traceback
 
 def worker():
     desubot.bot.run()
@@ -8,19 +9,24 @@ def worker():
 def main():
     desubot.bot.load_plugins('plugins')
     desubot.bot.join('#Moto-chan')
+    desubot.bot.join('#animu')
+    desubot.bot.join('#anime-planet.com')
     
     thread = threading.Thread(target=worker)
     thread.start()
 
     while True:
-        msg = input()
-        if msg.startswith(':'):
-            desubot.bot.load_plugins('plugins')
-        else:
-            desubot.bot.send(msg)
+        try:
+            msg = input()
+            if msg.startswith(':'):
+                desubot.bot.load_plugins('plugins')
+            else:
+                desubot.bot.send(msg)
+        except:
+            traceback.print_exc()
 
 if __name__ == '__main__':
     main()
 
 else:
-    bot = IRCBot('desutest', 'irc.rizon.net', command_prefix='!')
+    bot = IRCBot('desubot', 'irc.rizon.net', command_prefix='!')
