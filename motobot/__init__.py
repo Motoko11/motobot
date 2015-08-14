b4540d6ce4b5e73ede17729c06157953dfd848e9
Motoko11 <robertswaine7@gmail.com>
2015-08-14 01:31:18 +0100

    Makes motobot a package

---
+++ motobot/__init__.py
@@ -0,0 +1,3 @@
+from .irc_level import IRCLevel
+from .irc_message import IRCMessage
+from .irc_bot import IRCBot
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='motobot',
+    version='0.1',
+    packages=['motobot'],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3'
+    ]
+)
