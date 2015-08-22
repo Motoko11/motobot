c60707a5172d7f4a165333da8551244e51576805
Motoko11 <robertswaine7@gmail.com>
2015-08-22 19:28:04 +0100

    Made FML work

--- plugins/fml.py
+++ plugins/fml.py
@@ -1,7 +1,25 @@
 from motobot import command
 from requests import get
+from bs4 import BeautifulSoup
+
+
+fml_cache = []
+
+
+def load_fml():
+    url = 'http://www.fmylife.com/random'
+    bs = BeautifulSoup(get(url).text)
+
+    for div in bs.find_all('div', {'class', 'article'}, recursive=True):
+        fml_cache.append(div.find('p').text)
+
+
+def get_fml():
+    if len(fml_cache) == 0:
+        load_fml()
+    return fml_cache.pop(0)
 
 
 @command('fml')
 def fml_command(message, database):
-    return 'Provisional command for fml'
+    return get_fml()
