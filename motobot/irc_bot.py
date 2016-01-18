0bb69b7de0957bf798381f0cd93253afa9c59f73
Motoko11 <robertswaine7@gmail.com>
2016-01-18 21:10:34 +0000

    Added priority
    Userlevel was also removed, as it'll be the responsibility of the
    privmsg handler.

--- motobot/irc_bot.py
+++ motobot/irc_bot.py
@@ -1,5 +1,6 @@
 from .irc_message import IRCMessage
 from .irc_level import IRCLevel
+from .priority import Priority
 from .database import Database
 from socket import create_connection
 from importlib import import_module, reload
@@ -13,6 +14,12 @@ class IRCBot:
 
     """ IRCBot class, plug in and go! """
 
+    command_plugin = 1
+    match_plugin = 2
+    sink_plugin = 3
+    hook = 'motobot_hook'
+    plugin = 'motobot_plugin'
+
     def __init__(self, config):
         """ Create a new instance of IRCBot. """
         self.load_config(config)
@@ -22,11 +29,9 @@ class IRCBot:
         self.read_buffer = ''
 
         self.packages = []
-        self.plugins = {}
+        self.modules = {}
         self.hooks = {}
-        self.commands = {}
-        self.patterns = []
-        self.sinks = []
+        self.plugins = []
 
         self.channels = []
         self.ignore_list = []
@@ -78,9 +83,7 @@ class IRCBot:
     def reload_plugins(self):
         """ Reload all plugins from packages. """
         self.hooks = {}
-        self.commands = {}
-        self.patterns = []
-        self.sinks = []
+        self.plugins = []
 
         for package in self.packages:
             path = import_module(package).__path__._path
@@ -89,39 +92,28 @@ class IRCBot:
 
     def __load_module(self, module_name):
         """ Load or reload a module. """
-        if module_name in self.plugins:
-            reload(self.plugins[module_name])
+        if module_name in self.modules:
+            reload(self.modules[module_name])
             print("Module: {} reloaded".format(module_name))
         else:
-            self.plugins[module_name] = import_module(module_name)
+            self.modules[module_name] = import_module(module_name)
             print("Module: {} loaded".format(module_name))
 
-        module = self.plugins[module_name]
+        module = self.modules[module_name]
         for func in [getattr(module, attrib) for attrib in dir(module)]:
             self.__add_hook(func)
-            self.__add_command(func)
-            self.__add_pattern(func)
-            self.__add_sink(func)
+            self.__add_plugin(func)
+        sorted(self.plugins, key=lambda plugin: plugin[2])
 
     def __add_hook(self, func):
-        if hasattr(func, 'motobot_hook'):
-            for hook in func.motobot_hook:
-                self.hooks[hook] = func
+        """ Add a hook to the bot. """
+        for hook in getattr(func, IRCBot.hook, []):
+            self.hooks[hook] = func
 
-    def __add_command(self, func):
-        if hasattr(func, 'motobot_command'):
-            for command, level in func.motobot_command:
-                self.commands[command] = userlevel_wrapper(func, level)
-
-    def __add_pattern(self, func):
-        if hasattr(func, 'motobot_pattern'):
-            for pattern, level in func.motobot_pattern:
-                regex = re.compile(pattern, re.IGNORECASE)
-                self.patterns.append((regex, userlevel_wrapper(func, level)))
-
-    def __add_sink(self, func):
-        if hasattr(func, 'motobot_sink'):
-            self.sinks.append(func)
+    def __add_plugin(self, func):
+        """ Add a plugin to the bot. """
+        for plugin_data in getattr(func, IRCBot.plugin, []):
+            self.plugins.append((func,) + plugin_data)
 
     def load_database(self, path):
         self.database = Database(path)
@@ -196,45 +188,42 @@ class IRCBot:
                 print("Unknown command: {}".format(message.command))
 
 
-def userlevel_wrapper(func, level):
-    """ Modify a plugin to only take users above a certain userlevel. """
-    def wrapped(bot, message, *args, **kwargs):
-        if max(bot.userlevels.get((message.nick, message.channel), [IRCLevel.user])) >= level:
-            return func(bot, message, *args, **kwargs)
-    return wrapped
-
-
 def hook(command):
     """ Decorator to add a hook to the bot. """
     def register_hook(func):
-        if not hasattr(func, 'motobot_hook'):
-            func.motobot_hook = []
-        func.motobot_hook.append(command)
+        attr = getattr(func, IRCBot.hook, [])
+        attr.append(command)
+        setattr(func, IRCBot.hook, attr)
         return func
     return register_hook
 
 
-def command(name, level=IRCLevel.user):
+def command(name, level=IRCLevel.user, priority=Priority.medium):
     """ Decorator to add a command to the bot. """
     def register_command(func):
-        if not hasattr(func, 'motobot_command'):
-            func.motobot_command = []
-        func.motobot_command.append((name, level))
+        attr = getattr(func, IRCBot.plugin, [])
+        attr.append((IRCBot.command_plugin, priority, level, name))
+        setattr(func, IRCBot.plugin, attr)
         return func
     return register_command
 
 
-def match(pattern, level=IRCLevel.user):
+def match(pattern, level=IRCLevel.user, priority=Priority.medium):
     """ Decorator to add a regex pattern to the bot. """
     def register_pattern(func):
-        if not hasattr(func, 'motobot_pattern'):
-            func.motobot_pattern = []
-        func.motobot_pattern.append((pattern, level))
+        attr = getattr(func, IRCBot.plugin, [])
+        compiled = re.compile(pattern, re.IGNORECASE)
+        attr.append((IRCBot.match_plugin, priority, level, compiled))
+        setattr(func, IRCBot.plugin, attr)
         return func
     return register_pattern
 
 
-def sink(func):
+def sink(level=IRCLevel.user, priority=Priority.medium):
     """ Decorator to add sink to the bot. """
-    func.motobot_sink = True
-    return func
+    def register_sink(func):
+        attr = getattr(func, IRCBot.plugin, [])
+        attr.append((IRCBot.sink_plugin, priority, level))
+        setattr(func, IRCBot.plugin, attr)
+        return func
+    return register_sink
