2a3901857653c504fbbff6e98541a99ac265f943
Motoko11 <robertswaine7@gmail.com>
2016-01-18 21:10:52 +0000

    Handler function name changes

--- motobot/core_plugins/userlist_handlers.py
+++ motobot/core_plugins/userlist_handlers.py
@@ -3,25 +3,25 @@ from ..irc_level import get_userlevels
 
 
 @hook('353')
-def __handle_names(bot, message):
+def handle_names(bot, message):
     """ Parse the name command and record the userlevels of users. """
     pass
 
 
 @hook('JOIN')
-def __handle_join(bot, message):
+def handle_join(bot, message):
     """ Handle the join of a user. """
     pass
 
 
 @hook('MODE')
-def __handle_mode(bot, message):
+def handle_mode(bot, message):
     """ Handle the mode command and update userlevels accordingly. """
     pass
 
 
 @hook('PART')
 @hook('QUIT')
-def __handle_leave(bot, message):
+def handle_leave(bot, message):
     """ Handle the part or quit of a user. """
     pass
