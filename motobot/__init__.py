98c243b34e388a8898e952301f0f0fb45256d65e
Motoko11 <robertswaine7@gmail.com>
2016-01-18 21:11:13 +0000

    adds Priority class

--- motobot/__init__.py
+++ motobot/__init__.py
@@ -1,3 +1,4 @@
 from .irc_level import IRCLevel
 from .irc_message import IRCMessage, action
 from .irc_bot import IRCBot, hook, command, match, sink
+from .priority import Priority
---
+++ motobot/priority.py
@@ -0,0 +1,10 @@
+class Priority:
+    """ Enum class (Not really) for plugin priority. """
+    max = 7
+    highest = 6
+    higher = 5
+    medium = 4
+    low = 3
+    lower = 2
+    lowest = 1
+    min = 0
