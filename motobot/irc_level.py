5b5b7976165e7f72f16ca4cd9dce15ce4d5cc931
Motoko11 <robertswaine7@gmail.com>
2016-01-19 18:07:35 +0000

    Adds some aliases for backwards compat

--- motobot/irc_level.py
+++ motobot/irc_level.py
@@ -1,9 +1,9 @@
 class IRCLevel:
     """ Enum class (Not really) for userlevels. """
     user = 0
-    vop = 1
-    hop = 2
-    aop = 3
-    sop = 4
+    vop = voice = 1
+    hop = halfop = 2
+    aop = op = 3
+    sop = protop = 4
     owner = 5
     master = 6
