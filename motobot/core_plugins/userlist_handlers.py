999d6a95ad3fe50a4c0cb29f1c736f7962cd6425
Motoko11 <robertswaine7@gmail.com>
2016-01-19 19:03:06 +0000

    Added support for most commands which alter the userlevel list

--- motobot/core_plugins/userlist_handlers.py
+++ motobot/core_plugins/userlist_handlers.py
@@ -1,4 +1,23 @@
-from motobot import hook, IRCLevel
+from motobot import hook, IRCLevel, command
+from .privmsg_handlers import get_nick
+
+
+@command('levelprobe')
+def levelprobe_command(bot, nick, channel, message, args):
+    try:
+        mapping = {
+            IRCLevel.user: 'has nothing',
+            IRCLevel.voice: 'has a voice',
+            IRCLevel.hop: 'has a half-op',
+            IRCLevel.aop: 'has an ops',
+            IRCLevel.sop: 'has a protected ops',
+            IRCLevel.owner: 'is the owner'
+        }
+        return "{} {} in this channel.".format(args[1], mapping[max(bot.userlevels[(channel, args[1])])])
+    except IndexError:
+        return "Please supply a valid user to probe."
+    except KeyError:
+        return "Please supply a user who is actually in this channel."
 
 
 @hook('353')
@@ -37,17 +56,69 @@ def get_userlevels(name):
 @hook('JOIN')
 def handle_join(bot, message):
     """ Handle the join of a user. """
-    pass
+    nick = get_nick(message.sender)
+    channel = message.params[0]
+    bot.userlevels[(channel, nick)] = [IRCLevel.user]
+
+
+@hook('NICK')
+def handle_nick(bot, message):
+    """ Handle the nick change of a user. """
+    old_nick = get_nick(message.sender)
+    new_nick = message.params[0]
+
+    for channel, nick in bot.userlevels:
+        if nick == old_nick:
+            bot.userlevels[(channel, new_nick)] = \
+                bot.userlevels.pop((channel, nick))
 
 
 @hook('MODE')
 def handle_mode(bot, message):
     """ Handle the mode command and update userlevels accordingly. """
-    pass
+    mapping = {
+        'v': IRCLevel.voice,
+        'h': IRCLevel.hop,
+        'o': IRCLevel.aop,
+        'a': IRCLevel.sop,
+        'q': IRCLevel.owner
+    }
+    channel = message.params[0]
+    nicks = message.params[2:]
+    add = True if message.params[1][0] == '+' else False
+    modes = message.params[1][1:]
+
+    for nick, mode in zip(nicks, modes):
+        if mode in mapping:
+            level = mapping[mode]
+            userlevels = bot.userlevels[(channel, nick)]
+            if add:
+                userlevels.append(level)
+            else:
+                userlevels = [x for x in userlevels if x != level]
+            bot.userlevels[(channel, nick)] = userlevels
 
 
 @hook('PART')
-@hook('QUIT')
-def handle_leave(bot, message):
-    """ Handle the part or quit of a user. """
-    pass
+def handle_part(bot, message):
+    """ Handle the part of a user. """
+    nick = get_nick(message.sender)
+    channel = message.params[0]
+    bot.userlevels.pop((channel, nick))
+
+
+@hook('KICK')
+def handle_kick(bot, message):
+    """ Handle the kick of a user. """
+    nick = message.params[1]
+    channel = message.params[0]
+    bot.userlevels.pop((channel, nick))
+
+
+def handle_quit(bot, message):
+    """ Handle the quit of a user. """
+    quitting_nick = get_nick(message.sender)
+
+    for channel, nick in bot.userlevels:
+        if nick == quitting_nick:
+            bot.userlevels.pop((channel, nick))
