e010d63db17e80e8a173ed8a933879411410ff52
Motoko11 <robertswaine7@gmail.com>
2016-01-19 18:07:56 +0000

    Implement NAMES handler

--- motobot/core_plugins/userlist_handlers.py
+++ motobot/core_plugins/userlist_handlers.py
@@ -1,11 +1,37 @@
-from motobot import hook
-from ..irc_level import get_userlevels
+from motobot import hook, IRCLevel
 
 
 @hook('353')
 def handle_names(bot, message):
     """ Parse the name command and record the userlevels of users. """
-    pass
+    channel = message.params[2]
+    names = message.params[-1].split(' ')
+    for name in names:
+        handle_name(bot, channel, name)
+
+
+def handle_name(bot, channel, name):
+    """ Handle a single name from the name command. """
+    userlevels, nick = get_userlevels(name)
+    bot.userlevels[(channel, nick)] = userlevels
+
+
+def get_userlevels(name):
+    """ Get the userlevel from a nick and return the userlevel and nick. """
+    mapping = {
+        '+': IRCLevel.voice,
+        '%': IRCLevel.hop,
+        '@': IRCLevel.aop,
+        '&': IRCLevel.sop,
+        '~': IRCLevel.owner
+    }
+    userlevels = [IRCLevel.user]
+
+    for i in range(len(name)):
+        if name[i] in mapping:
+            userlevels.append(mapping[name[i]])
+        else:
+            return userlevels, name[i:]
 
 
 @hook('JOIN')
