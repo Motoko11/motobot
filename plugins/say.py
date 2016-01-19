deda647d7edc28c5668ccdf6eb1a8b0d154a3cb0
Motoko11 <robertswaine7@gmail.com>
2016-01-19 22:17:38 +0000

    Bugfix from changeover

--- plugins/say.py
+++ plugins/say.py
@@ -3,14 +3,11 @@ from motobot import command, action, IRCLevel
 
 @command('say', level=IRCLevel.master)
 def say_command(bot, nick, channel, message, args):
-    if nick.lower() not in [x.lower() for x in masters]:
-        return "Check your privilege!"
+    if len(args) < 3:
+        return "You must specify both a channel and a message"
     else:
-        if len(args) < 3:
-            return "You must specify both a channel and a message"
-        else:
-            channel = args[1]
-            message = ' '.join(args[2:])
-            if message.startswith('/me '):
-                message = action(message[4:])
-            bot.send('PRIVMSG {} :{}'.format(channel, message))
+        channel = args[1]
+        message = ' '.join(args[2:])
+        if message.startswith('/me '):
+            message = action(message[4:])
+        bot.send('PRIVMSG {} :{}'.format(channel, message))
