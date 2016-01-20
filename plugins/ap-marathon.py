9aadeeec6bdec1fcf18008ae0b15ad6737f28d97
Motoko11 <robertswaine7@gmail.com>
2016-01-20 23:13:06 +0000

    Updates marathon to use new API endpoint
    Thank you Chala :3

--- plugins/ap-marathon.py
+++ plugins/ap-marathon.py
@@ -5,18 +5,6 @@ from time import time
 from re import sub
 
 
-url = 'https://marathon.chalamius.se/'
-
-
-marathon_cache = {
-    'time': 0,
-    'title': '',
-    'date': '',
-    'link': '',
-    'note': ''
-}
-
-
 @command('marathonlist')
 def marathonlist_command(bot, nick, channel, message, args):
     return "The marathon list can be found at {}.".format(url)
@@ -31,30 +19,10 @@ def marathon_command(bot, nick, channel, message, args):
 
 
 def get_current_marathon():
-    global marathon_cache
-
-    if marathon_cache['time'] < time():
-        bs = BeautifulSoup(get(url).text)
-
-        entries = bs.find_all('tr', recursive=True)
-        entry = entries[-1]
-
-        title_cell = entry.find('td', {'class', 'anime-title'})
-        date_call = entry.find('td', {'class', 'anime-date'})
-        note_cell = entry.find('td', {'class', 'anime-note'})
-
-        marathon_cache['title'] = chala_protection(title_cell.text)
-        marathon_cache['date'] = chala_protection(date_call.text)
-        marathon_cache['link'] = chala_protection(title_cell.find('a')['href'])
-        marathon_cache['note'] = chala_protection(note_cell.text)
-        marathon_cache['time'] = time() + 60 * 60
-
-    return (marathon_cache['title'], marathon_cache['date'],
-            marathon_cache['link'], marathon_cache['note'])
-
-
-def chala_protection(string):
-    return sub(r'[^\S ]+', '', string).strip()
+    url = 'https://marathon.chalamius.se/calendar.json'
+    entries = get(url).json()['items']
+    entry = entries[-1]
+    return entry['name'], entry['date'], entry['url'], entry['note']
 
 
 @command('pantsu')
