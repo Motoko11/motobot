878b859da5f4cc5de85b93e772a3cfc786f5961f
Motoko11 <robertswaine7@gmail.com>
2016-01-21 01:09:33 +0000

    Remove Noda's silly trigger

--- plugins/nodame.py
+++
@@ -1,9 +0,0 @@
-from motobot import match
-from random import randint
-
-
-@match(r'hentai')
-def noda_match(bot, nick, channel, message, match):
-    valid = ['nodame-chan', 'nodanyan', 'moto-chan', 'otakuhime']
-    if nick.lower() in valid:
-        return 'hentai' * randint(1, 15)
