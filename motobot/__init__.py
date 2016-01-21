01f2971600c6a5c1d8fc24ab1e3769174a522830
Motoko11 <robertswaine7@gmail.com>
2016-01-21 20:52:57 +0000

    Adds modifiers to Motobot __init__

--- motobot/__init__.py
+++ motobot/__init__.py
@@ -2,3 +2,4 @@ from .irc_level import IRCLevel
 from .irc_message import IRCMessage, action
 from .irc_bot import IRCBot, hook, command, match, sink
 from .priority import Priority
+from .modifiers.plugin_modifiers import Action, Target, Command
