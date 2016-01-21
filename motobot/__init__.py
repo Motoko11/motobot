a8646ba6db6c415bfaa87110b63a9dbc6a7c430f
Motoko11 <robertswaine7@gmail.com>
2016-01-21 21:51:26 +0000

    Removes the action hack command

--- motobot/__init__.py
+++ motobot/__init__.py
@@ -1,5 +1,5 @@
 from .irc_level import IRCLevel
-from .irc_message import IRCMessage, action
+from .irc_message import IRCMessage
 from .irc_bot import IRCBot, hook, command, match, sink
 from .priority import Priority
 from .modifiers.modifier import Modifier
