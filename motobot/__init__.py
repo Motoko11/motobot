84b5f120df0d829e56856629721603ab33c659f7
Motoko11 <robertswaine7@gmail.com>
2016-01-21 20:54:15 +0000

    Add Modifier to Motobot __init__

--- motobot/__init__.py
+++ motobot/__init__.py
@@ -2,4 +2,5 @@ from .irc_level import IRCLevel
 from .irc_message import IRCMessage, action
 from .irc_bot import IRCBot, hook, command, match, sink
 from .priority import Priority
+from .modifiers.modifier import Modifier
 from .modifiers.plugin_modifiers import Action, Target, Command
