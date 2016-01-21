25149ced5aa015b30f6ec58fb2e3300b677a19b7
Motoko11 <robertswaine7@gmail.com>
2016-01-21 22:06:37 +0000

    update say to work with modifiers

--- plugins/say.py
+++ plugins/say.py
@@ -1,13 +1,16 @@
-from motobot import command, action, IRCLevel
+from motobot import command, Action, Target, IRCLevel
 
 
 @command('say', level=IRCLevel.master)
 def say_command(bot, nick, channel, message, args):
-    if len(args) < 3:
-        return "You must specify both a channel and a message"
-    else:
-        channel = args[1]
-        message = ' '.join(args[2:])
+    try:
+        target = Target(args[1])
+        msg = ' '.join(args[2:])
+
         if message.startswith('/me '):
-            message = action(message[4:])
-        bot.send('PRIVMSG {} :{}'.format(channel, message))
+            return msg, target, Action
+        else:
+            return msg, target
+
+    except IndexError:
+        return "You must specify both a channel and a message"
