92573aaecdbe243d44525ea19502348070a8cdbf
Motoko11 <robertswaine7@gmail.com>
2016-01-23 20:00:10 +0000

    Adds temporary join command

---
+++ motobot/core_plugins/channel_manager.py
@@ -0,0 +1,21 @@
+from motobot import hook, command, Command
+from time import sleep
+
+
+@command('join')
+def join_command(bot, nick, channel, message, args):
+    try:
+        channel = args[1]
+        bot.channels.append(channel)
+        return Command('JOIN', [channel])
+    except IndexError:
+        return "Please provide a channel."
+
+
+@hook('KICK')
+def kick_hook(bot, message):
+    nick = message.params[1]
+    if nick == bot.nick:
+        sleep(1)
+        channel = message.params[0]
+        bot.send('JOIN ' + channel)
