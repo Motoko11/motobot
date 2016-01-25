a3efbddf34c9f25355dbe2e831d2c32b5ed6f91d
Motoko11 <robertswaine7@gmail.com>
2016-01-25 12:05:28 +0000

    Change to new database object + Entry

--- motobot/database.py
+++ motobot/database.py
@@ -1,31 +1,49 @@
-from json import dump, load
-from os import remove, rename
+from pickle import load, dump, HIGHEST_PROTOCOL
+from os import replace
+
+
+class Entry:
+    def __init__(self, database, data={}):
+        self.__database = database
+        self.__data = data
+
+    def get_val(self, name, default=None):
+        return self.__data.get(name, default)
+
+    def set_val(self, name, value):
+        self.__data[name] = value
+        self.__database.changed = True
+
+    def is_empty(self):
+        return self.__data == {}
 
 
 class Database:
     def __init__(self, database_path=None):
         self.database_path = database_path
         self.data = {}
+        self.changed = False
         self.load_database()
 
     def load_database(self):
         if self.database_path is not None:
             try:
-                f = open(self.database_path, 'r')
-                self.data = load(f)
-            except:
-                open(self.database_path, 'a').close()
+                file = open(self.database_path, 'rb')
+                self.data = load(file)
+            except FileNotFoundError:
+                self.changed = True
+                self.write_database()
 
-    def get_val(self, name, default=None):
-        return self.data.get(name, default)
+    def write_database(self):
+        if self.database_path is not None and self.changed:
+            temp_path = self.database_path + '.temp'
+            file = open(temp_path, 'wb')
+            dump(self.data, file, HIGHEST_PROTOCOL)
+            file.close()
+            replace(temp_path, self.database_path)
+            self.changed = False
 
-    def set_val(self, name, val):
-        self.data[name] = val
-
-        if self.database_path is not None:
-            temp_name = self.database_path + '.temp'
-            f = open(temp_name, 'w')
-            dump(self.data, f, indent=2)
-            f.close()
-            remove(self.database_path)
-            rename(temp_name, self.database_path)
+    def get_entry(self, name):
+        if name not in self.data:
+            self.data[name] = Entry(self)
+        return self.data[name]
