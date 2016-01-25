bd1498d35ab099c40d13d97db1caf68019540d0e
Motoko11 <robertswaine7@gmail.com>
2016-01-25 10:36:57 +0000

    Move modifiers to a single module

--- motobot/__init__.py
+++ motobot/__init__.py
@@ -2,6 +2,4 @@ from .irc_level import IRCLevel
 from .irc_message import IRCMessage
 from .irc_bot import IRCBot, hook, command, match, sink
 from .priority import Priority
-from .modifiers.modifier import Modifier
-from .modifiers.plugin_modifiers import Action, Target, Command
-from .modifiers.eat import EatModifier, Eat
+from .modifiers import Modifier, Action, Target, Command, EatModifier, Eat
