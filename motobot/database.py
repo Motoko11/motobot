e54a25b44b15482979ce4fb21aad8f28996f3bba
Motoko11 <robertswaine7@gmail.com>
2016-01-26 20:40:11 +0000

    Fix database bugs

--- motobot/database.py
+++ motobot/database.py
@@ -3,8 +3,7 @@ from os import replace
 
 
 class DatabaseEntry:
-    def __init__(self, database):
-        self.__database = database
+    def __init__(self):
         self.__data = None
 
     def get_val(self, default=None):
@@ -12,7 +11,6 @@ class DatabaseEntry:
 
     def set_val(self, value):
         self.__data = value
-        self.__database.changed = True
 
 
 class Database:
@@ -32,7 +30,7 @@ class Database:
                 self.write_database()
 
     def write_database(self):
-        if self.database_path is not None and self.changed:
+        if self.database_path is not None:
             temp_path = self.database_path + '.temp'
             file = open(temp_path, 'wb')
             dump(self.data, file, HIGHEST_PROTOCOL)
@@ -42,5 +40,5 @@ class Database:
 
     def get_entry(self, name):
         if name not in self.data:
-            self.data[name] = DatabaseEntry(self)
+            self.data[name] = DatabaseEntry()
         return self.data[name]
