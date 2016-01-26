06f14baee2e4171b4a7ae4b3db346a2033fb2554
Motoko11 <robertswaine7@gmail.com>
2016-01-26 04:20:40 +0000

    Add help plugin

---
+++ motobot/core_plugins/help.py
@@ -0,0 +1,38 @@
+from motobot import IRCBot, command#, Command
+
+
+def get_command_help(bot, command):
+    responses = []
+
+    func = lambda x: x.type == IRCBot.command_plugin and x.arg.lower() == command.lower()
+    for plugin in filter(func, bot.plugins):
+        func = plugin.func
+        help_text = getattr(func, IRCBot.help_text, None)
+
+        if help_text is not None:
+            responses.append(help_text)
+        elif func.__doc__ is not None:
+            responses.append(' '.join(func.__doc__.split()))
+    return responses
+
+
+@command('help')
+def help_command(bot, database, nick, channel, message, args):
+    """ Print help messages for the user.
+
+    Takes a single argument for a command name.
+    No arguments gives a generic help message.
+    """
+    response = None
+
+    if len(args) <= 1:
+        default_help = "For help on a specific command use '!help command'."
+        response = bot.default_help \
+            if bot.default_help is not None else default_help
+    else:
+        response = get_command_help(bot, args[1])
+
+        if response == []:
+            response = "There is no help entry for the command: {}.".format(args[1])
+
+    return response
