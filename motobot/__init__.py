9012f52d98b1d2a32a2ad2674f1dc2bc7b211e5b
Motoko11 <robertswaine7@gmail.com>
2016-01-26 04:21:41 +0000

    Remove decorators from IRCBot

--- motobot/__init__.py
+++ motobot/__init__.py
@@ -1,5 +1,6 @@
 from .irc_level import IRCLevel
 from .irc_message import IRCMessage
-from .irc_bot import IRCBot, hook, command, match, sink
+from .irc_bot import IRCBot
+from .decorators import hook, command, match, sink
 from .priority import Priority
 from .modifiers import Modifier, Action, Target, Command, EatModifier, Eat
