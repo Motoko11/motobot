faa3597552439705181ab8d0a051f62807712e1d
Motoko11 <robertswaine7@gmail.com>
2016-01-27 03:55:58 +0000

    Adds message and error checking

--- motobot/core_plugins/bot_management.py
+++ motobot/core_plugins/bot_management.py
@@ -38,7 +38,10 @@ def command_command(bot, database, nick, channel, message, args):
             value = args[3:]
             response = set_val(bot, name, value, notice)
         elif arg == 'reload':
-            bot.reload_plugins()
+            error = bot.reload_plugins()
+            response = ("Plugins have been reloaded.", notice)
+            if error:
+                response[0] += " There were some errors."
         else:
             response = ("Error: Invalid argument.", notice)
     except IndexError:
