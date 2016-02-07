1c537cc0eb7e4c3e1350765ce8fbefdaba46ac21
Motoko11 <robertswaine7@gmail.com>
2016-02-07 22:26:40 +0000

    Adds commands command

---
+++ motobot/core_plugins/commands.py
@@ -0,0 +1,23 @@
+from motobot import command, Notice, format_responses, IRCBot
+
+
+@command('commands')
+def commands_command(bot, database, nick, channel, message, args):
+    userlevel = bot.get_userlevel(channel, nick)
+
+    valid_command = lambda plugin: plugin.type == IRCBot.command_plugin \
+        and plugin.level <= userlevel and not plugin.arg.hidden
+    key = lambda plugin: (plugin.arg.trigger, plugin.func)
+
+    command_groups = {}
+    for command, func in map(key, filter(valid_command, bot.plugins)):
+        value = command_groups.get(func, [])
+        value.append(command)
+        command_groups[func] = value
+
+    format_group = lambda group: '({})'.format(', '.join(group)) \
+        if len(group) != 1 else group[0]
+    commands = map(format_group, sorted(command_groups.values(), key=lambda x: x[0]))
+    response = format_responses(commands, "Bot Commands: {};")
+
+    return response, Notice(nick)
