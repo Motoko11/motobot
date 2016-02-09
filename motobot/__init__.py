53fe1c262420e1c7a29b1db3c0b320f706035a78
Motoko11 <robertswaine7@gmail.com>
2016-02-09 02:06:15 +0000

    Adds CTCP modifier

--- motobot/__init__.py
+++ motobot/__init__.py
@@ -3,5 +3,5 @@ from .irc_message import IRCMessage
 from .irc_bot import IRCBot
 from .decorators import hook, command, match, sink
 from .priority import Priority
-from .modifiers import Modifier, Action, Target, Command, Notice, EatModifier, Eat
+from .modifiers import Modifier, Action, CTCP, Target, Command, Notice, EatModifier, Eat
 from .utilities import format_responses
