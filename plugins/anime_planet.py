f9a5f049b9bba04dec695c9e5ac1a43e0f8565e7
Motoko11 <robertswaine7@gmail.com>
2016-02-21 19:05:26 +0000

    Changes the rules link

--- plugins/anime_planet.py
+++ plugins/anime_planet.py
@@ -18,7 +18,7 @@ def stats_command(bot, context, message, args):
 @command('rr')
 def rr_command(bot, context, message, args):
     """ Return the recommendations response. Takes single arg for target. """
-    response = "If you are looking for anime/manga recommendations we have a database created specifically for that! Just visit www.anime-planet.com and let us do the hard work for you! For channel rules, please go to http://allspark.net/cypherswipe/animeplanet-rules.html"
+    response = "If you are looking for anime/manga recommendations we have a database created specifically for that! Just visit www.anime-planet.com and let us do the hard work for you! For channel rules, please go to http://bit.ly/1L1tnfV"
 
     if len(args) > 1:
         response = "{}: {}".format(' '.join(args[1:]).strip(), response)
