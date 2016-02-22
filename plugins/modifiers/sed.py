a87bc30d62b543891a675434b34e5c333bd66efa
Motoko11 <robertswaine7@gmail.com>
2016-02-22 05:20:57 +0000

    Adds a (lazy) sed module

---
+++ plugins/modifiers/sed.py
@@ -0,0 +1,15 @@
+from motobot import command
+from codecs import encode
+from re import compile, IGNORECASE
+
+
+sed_pattern = compile(r'^(?:.+)sed(?: ?)(?:s?)\/(.*?)\/(.*?)\/(?:.*?) (.+)$', IGNORECASE)
+
+
+@command('sed')
+def sed_command(bot, context, message, args):
+    match = sed_pattern.match(message)
+    if match is not None:
+        pattern, replace, arg = match.groups()
+        pattern = compile(pattern, IGNORECASE)
+        return pattern.sub(replace, arg)
