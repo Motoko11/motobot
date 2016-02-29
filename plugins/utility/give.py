2ea0ecc39c0ab75030251e030309ac31e7e3a488
Motoko11 <robertswaine7@gmail.com>
2016-02-29 10:14:05 +0000

    remove desubot files from core repo

--- plugins/utility/give.py
+++
@@ -1,8 +0,0 @@
-from motobot import command
-
-
-@command('give')
-def give_command(bot, context, message, args):
-    target = args[1]
-    message = ' '.join(args[2:])
-    return "{}: {}".format(target, message)
