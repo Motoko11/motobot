21a7791aa413e9f677b706a2ac3aaf858a3d5bf4
Motoko11 <robertswaine7@gmail.com>
2016-03-04 09:12:14 +0000

    Remove modifier bases from __init__

--- motobot/__init__.py
+++ motobot/__init__.py
@@ -3,7 +3,7 @@ from .irc_message import IRCMessage
 from .irc_bot import IRCBot
 from .decorators import hook, command, match, sink, request
 from .priority import Priority
-from .modifiers import Modifier, Action, CTCP, Target, Command, Notice, EatModifier, Eat
-from .utilities import split_response, Context
+from .modifiers import Action, CTCP, Target, Command, Notice, Eat
+from .utilities import strip_control_codes, split_response, Context
 
 __VERSION__ = '0.2'
