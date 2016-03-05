2d2b1b770a3016170be445d59532c76491cfb67e
Motoko11 <robertswaine7@gmail.com>
2016-03-05 20:46:59 +0000

    Style update

--- motobot/core_plugins/bot_management.py
+++ motobot/core_plugins/bot_management.py
@@ -12,8 +12,6 @@ def command_command(bot, context, message, args):
     The 'set' argument will set an attribute of the bot. Use with caution.
     The 'reload' command will reload the plugins in the loaded packages.
     """
-    response = None
-
     try:
         arg = args[1].lower()
 
