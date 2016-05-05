f6c0ec80a1abe31edd3115b479f6b262184e9007
Motoko11 <robertswaine7@gmail.com>
2016-05-05 05:24:02 +0100

    Make command not implicit

--- motobot/core_plugins/channel_override.py
+++ motobot/core_plugins/channel_override.py
@@ -6,7 +6,7 @@ def channel_command(bot, context, message, args):
     """ Override the channel to make a command act as if it were in another channel. """
     try:
         channel = args[1]
-        message = '!' + ' '.join(args[2:])
+        message = ' '.join(args[2:])
         return bot.request('HANDLE_MESSAGE', context.nick, channel, context.host, message)
     except IndexError:
         return "Error: Please provide a channel.", Notice(context.nick)
