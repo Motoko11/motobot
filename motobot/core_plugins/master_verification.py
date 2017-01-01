e75209588b117e0f48d3014b69f1144306d7dd47
Motoko11 <robertswaine7@gmail.com>
2017-01-01 22:19:29 +0000

    Adds adminmask command

--- motobot/core_plugins/master_verification.py
+++ motobot/core_plugins/master_verification.py
@@ -1,4 +1,4 @@
-from motobot import hook, request, command, BotError, Notice, hostmask_check
+from motobot import hook, request, command, BotError, Notice, hostmask_check, Priority, IRCLevel, split_response
 
 
 @request('IS_MASTER')
@@ -9,7 +9,7 @@ def is_master_request(bot, context, nick, host=None):
            any(hostmask_check(nick, host, mask) for mask in admin_masks)
 
 
-@command('identify', hidden=True)
+@command('identify', hidden=True, priority=Priority.max)
 def identify_command(bot, context, message, args):
     try:
         password = bot.admin_pass
@@ -29,3 +29,53 @@ def recognise(session, nick):
     admins = session.get(set())
     admins.add(nick.lower())
     session.set(admins)
+
+
+@command('adminmask')
+def adminmask_command(bot, context, message, args):
+    try:
+        arg = args[1].lower()
+        if arg == 'add':
+            response = add_mask(context.database, args[2])
+        elif arg in ('del', 'rem'):
+            response = del_mask(context.database, args[2])
+        elif arg in ('show', 'list'):
+            response = show_masks(context.database)
+        else:
+            response = 'Error: Invalid argument;'
+    except IndexError:
+        response = "Not enough arguments provided."
+    return response, Notice(context.nick)
+
+
+def add_mask(database, mask):
+    masks = database.get([])
+    if mask not in masks:
+        masks.append(mask)
+        database.set(masks)
+        response = "Mask added successfully."
+    else:
+        response = "Error: Mask already in masks list."
+    return response
+
+
+def del_mask(database, mask):
+    masks = database.get([])
+    try:
+        masks.remove(mask)
+        database.set(masks)
+        response = "Mask successfully deleted."
+    except ValueError:
+        response = "Error: Mask not in masks list."
+    return response
+
+
+def show_masks(database):
+    masks = database.get([])
+
+    if masks:
+        response = split_response(masks, "Admin masks: {};")
+    else:
+        response = "I don't have any admin masks."
+
+    return response
