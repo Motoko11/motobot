d19e0deb689fc683ea444e99ebbc9dcb129f9b4b
Motoko11 <robertswaine7@gmail.com>
2017-01-01 22:47:32 +0000

    Fix CTCP ping

--- motobot/core_plugins/ctcp.py
+++ motobot/core_plugins/ctcp.py
@@ -8,12 +8,12 @@ def ctcp_match(bot, context, message, match):
         'VERSION': 'MotoBot Version ' + __VERSION__,
         'FINGER': 'Oh you dirty man!',
         'TIME': strftime('%a %b %d %H:%M:%S UTC', gmtime()),
-        'PING': message
+        'PING': message.strip('\x01')
     }
     try:
-        ctcp_req = match.group(1)
+        ctcp_req = match.group(1).split(' ', 1)[0]
         reply = ctcp_response[ctcp_req]
-        response = (reply, Notice(context.nick), Eat)
+        response = ('\x01{}\x01'.format(reply), Notice(context.nick), Eat)
     except KeyError:
         response = None
     return response
